b8d50268a85e4746fb7d7817a5311120fe535ff3
ftcuser <rupesh.kumar@citizant.com>
2019-03-21 22:21:59 -0400

    Update TestController.java

--- src/main/java/com/kodcu/controller/TestController.java
+++ src/main/java/com/kodcu/controller/TestController.java
@@ -2,7 +2,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javainuse.model.Employee;
+import com.kodcu.model.Employee;
 
 @RestController
 public class TestController {
