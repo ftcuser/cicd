8bfafc61394669d0ca5f8fec64feef36f4ffb5e9
ftcuser <rupesh.kumar@citizant.com>
2019-03-22 00:39:04 -0400

    Update HelloWorldController.java

--- src/main/java/com/kodcu/controller/HelloWorldController.java
+++ src/main/java/com/kodcu/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello(){
         return "hello world";
     }
