cdc108af32c6cf26e73269d51978760c48d660c7
ftcuser <rupesh.kumar@citizant.com>
2019-03-22 00:18:10 -0400

    Update HelloWorldControllerTest.java

--- src/test/java/com/kodcu/HelloWorldControllerTest.java
+++ src/test/java/com/kodcu/HelloWorldControllerTest.java
@@ -1,5 +1,5 @@
 package com.kodcu;
-/*
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-*/
+/*
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,7 +32,7 @@ public class HelloWorldControllerTest {
 	}
 
 }
-/*
+*/
 public class HelloWorldControllerTest {
 
     @Autowired
