18797d1d404df12cf4bce8a7cc5deb4ffb31fa94
ftcuser <rupesh.kumar@citizant.com>
2019-03-22 00:10:41 -0400

    Update HelloWorldControllerTest.java

--- src/test/java/com/kodcu/HelloWorldControllerTest.java
+++ src/test/java/com/kodcu/HelloWorldControllerTest.java
@@ -1,5 +1,5 @@
 package com.kodcu;
-
+/*
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,9 +13,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+*/
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = SpringBootHelloWorldApplication.class )
 public class HelloWorldControllerTest {
 
