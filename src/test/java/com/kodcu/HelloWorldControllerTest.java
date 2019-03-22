a4a2d2c76f1fe856f2a6479981db42ae8f1a195c
ftcuser <rupesh.kumar@citizant.com>
2019-03-22 00:09:16 -0400

    Update HelloWorldControllerTest.java

--- src/test/java/com/kodcu/HelloWorldControllerTest.java
+++ src/test/java/com/kodcu/HelloWorldControllerTest.java
@@ -1,4 +1,4 @@
-package com.kodcu.controller;
+package com.kodcu;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
