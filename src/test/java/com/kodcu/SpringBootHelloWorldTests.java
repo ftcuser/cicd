23445b9bef12771f0f2b0955c6eaef044cb9c29d
ftcuser <rupesh.kumar@citizant.com>
2019-03-21 22:47:07 -0400

    Update SpringBootHelloWorldTests.java

--- src/test/java/com/kodcu/SpringBootHelloWorldTests.java
+++ src/test/java/com/kodcu/SpringBootHelloWorldTests.java
@@ -5,6 +5,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration
 public class SpringBootHelloWorldTests {
 
 	@Test
